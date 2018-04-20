9f2a282400881fc01558060f3920c78bd9d6240b
Felixvl <digitaleservice@gmail.com>
2018-04-20 10:38:03 +0200

    tweede doelstelling gehaald

--- index.php
+++ index.php
@@ -3,13 +3,27 @@
 $xlinks = 4;
 $xrechts = 5;
 
-echo "Blauw met vier".berekenBlauw($xlinks);
-echo "<br>";
-echo "Groen met vier".berekenGroen($xlinks);
-echo "<hr>";
-echo "Blauw met vijf".berekenBlauw($xrechts);
-echo "<br>";
-echo "Groen met vijf".berekenGroen($xrechts);
+echo "verschil xlinks: $xlinks :".verschilBerekenen($xlinks);
+echo "<br>";        
+echo "verschil xrechts: $xrechts : ".verschilBerekenen($xrechts);
+echo "<br>";        
+
+function verschilBerekenen($getal){
+    $verschil = berekenBlauw($getal) - berekenGroen($getal);
+    return $verschil;
+}
+
+if(verschilBerekenen($xrechts) < 0){
+    echo "$xlinks verschil is NEGATIEF";
+}else{
+    echo "$xlinks verschil is Positief";
+    
+}
+
+//functie (links )//bereken beide punten // retourneert verschil
+////$array
+//function (links rechts) //beide uitrekenen // dan verschil tussen // push naar array
+
 
 
 function berekenBlauw($getal){
@@ -22,5 +36,13 @@ function berekenGroen($getal){
 }
 
 
+
+echo "Blauw met vier".berekenBlauw($xlinks);
+echo "<br>";
+echo "Groen met vier".berekenGroen($xlinks);
+echo "<hr>";
+echo "Blauw met vijf".berekenBlauw($xrechts);
+echo "<br>";
+echo "Groen met vijf".berekenGroen($xrechts);
 
 ?>
